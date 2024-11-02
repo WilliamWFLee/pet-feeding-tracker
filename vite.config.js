567a11a7ea89c6ea81d688e9dde5522ee94d939c
William Lee <wlee753159@gmail.com>
2024-11-02 22:44:01 +0000

    Add vue-pug-plugin

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,19 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vuePugPlugin from 'vue-pug-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: {
+        preprocessOptions: {
+          plugins: [vuePugPlugin],
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),

da56fa00dbad376ad882d304bf6033bf2c1be2c0
William Lee <wlee753159@gmail.com>
2024-11-02 22:45:46 +0000

    Add button components

---
+++ src/components/LinkButton.vue
@@ -0,0 +1,8 @@
+<script setup>
+defineProps(['href', 'class'])
+</script>
+
+<template lang="pug">
+  a.px-2.py-3.rounded-md.align-middle(:href, :class)
+    slot
+</template>
---
+++ src/components/SecondaryLinkButton.vue
@@ -0,0 +1,10 @@
+<script setup>
+import LinkButton from './LinkButton.vue'
+
+defineProps(['href'])
+</script>
+
+<template lang="pug">
+  LinkButton(:href class='bg-rose-taupe text-magnolia')
+    slot
+</template>
